da85596c6aa330e6e6ebfd960230de088bc6d69d
omshe <86169236+omshe@users.noreply.github.com>
2026-09-24 14:15:46 -0400

    MyHashing exercise completed

--- exercises/ex06-overloading/src/main/java/MyHashing.java
+++ exercises/ex06-overloading/src/main/java/MyHashing.java
@@ -20,6 +20,7 @@ public class MyHashing {
   /** Creates a MyHashing whose seed starts at 0. */
   public MyHashing() {
     // TODO: this constructor takes no arguments; leave the seed at its default.
+    this.seed = 0;
   }
 
   /**
@@ -29,6 +30,7 @@ public class MyHashing {
    */
   public MyHashing(int seed) {
     // TODO: store the parameter in this object's seed field.
+    this.seed = seed;
   }
 
   /**
@@ -39,7 +41,9 @@ public class MyHashing {
    */
   public int hash(int value) {
     // TODO
-    return 0;
+    int prev = this.seed;
+    this.seed = value;
+    return prev;
   }
 
   /**
@@ -52,7 +56,9 @@ public class MyHashing {
    */
   public int hash(char value) {
     // TODO
-    return 0;
+    int to_return = (this.seed + value) % MODULO;
+    this.seed = value;
+    return to_return;
   }
 
   /**
@@ -65,6 +71,10 @@ public class MyHashing {
    */
   public static int hash(String value) {
     // TODO: String.toCharArray() may help.
-    return 0;
+    int sum = 0;
+    for (char c: value.toCharArray()){
+      sum += c;
+    }
+    return sum;
   }
 }
